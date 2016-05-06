701f5d4f31cc056c5fdcca277dc707c48986b373
jicjjang <jicjjang12@gmail.com>
2016-05-06 19:14:50 +0900

    add type print parts

--- testImageConverter.py
+++ testImageConverter.py
@@ -1,5 +1,11 @@
 from ImageConverter import ImageToCSV
 
-itc = ImageToCSV(200, "img1/")
+itc = ImageToCSV(886, "img1/")
 itc.set_training_data()
 itc.save_reduce_image_to_csv('data')
+
+print "type0: " + str(itc.type0)
+print "type1: " + str(itc.type1)
+print "type2: " + str(itc.type2)
+print "type3: " + str(itc.type3)
+print "type4: " + str(itc.type4)
